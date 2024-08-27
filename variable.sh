1eee49dfa93f777ec282590be1d9e3792f94dc62
jayakrishnakavalinehaan@gmail.com <jayakrishnakavalinehaan@gmail.com>
2024-08-27 13:31:49 +0530

    I wrote a variable script

---
+++ variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1  
+PERSON2=$2
+
+echo "$PERSON1 :: Hi ${PERSON2}, How are you?"
+echo "${PERSON2} :: Hello  $PERSON1. Iam fine. How are you doing?"
+echo "$PERSON1 :: Iam doing good ${PERSON2}, Whats going on?"
+echo "${PERSON2} :: I Started Learning Shell Script $Person1"
